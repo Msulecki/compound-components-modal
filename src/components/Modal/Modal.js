95b9c94107b4a2d1ea99d789664cc12219102894
Mateusz Sulecki <mat.sulecki@gmail.com>
2020-05-05 20:09:38 +0200

    added toggle method, added isOpen render condition

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -6,20 +6,34 @@ import '../../styles/Modal.scss';
 
 class Modal extends Component {
 
+    state = {
+        isOpened: false,
+    }
+
     static Content = Content;
     static Footer = Footer;
     static Header = Header;
 
+    toggle = () => {
+        this.setState({
+            isOpened: !this.state.isOpened
+        })
+    }
+
     render() {
-        const { toggle } = this.props;
-        const children = React.Children.map(this.props.children, child => React.cloneElement(child));
+        const { isOpened } = this.state;
+        const children = React.Children.map(this.props.children, child => React.cloneElement(child, { toggle: this.toggle }));
 
         return (
-            <div id="modal" className="modal" onClick={e => e.target.id === 'modal' && toggle(false)}>
-                <div className="modal__content">
-                    {children}
-                </div>
-            </div>
+            <>
+                {isOpened ?
+                    <div id="modal" className="modal" onClick={e => e.target.id === 'modal' && this.toggle()}>
+                        <div className="modal__content">
+                            {children}
+                        </div>
+                    </div>
+                    : <button className="btn" onClick={() => this.toggle()}>Toggle modal</button>}
+            </>
         );
     }
 }
